918a131d3fa7f4a978836c4be92476aeb9cea665
pmgalves <pmgalves@55ec6a14-0f31-11df-9e5d-c9c78cbd7d0e>
2010-02-01 17:59:43 +0000

    - Initial commit

---
+++ src/pt/webdetails/cda/CdaExecutor.java
@@ -0,0 +1,48 @@
+package pt.webdetails.cda;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Main class to test and execute the CDA in standalone mode
+ * User: pedro
+ * Date: Feb 1, 2010
+ * Time: 12:30:41 PM
+ */
+public class CdaExecutor {
+
+  private static final Log logger = LogFactory.getLog(CdaExecutor.class);
+  private static CdaExecutor _instance;
+
+
+  public CdaExecutor() {
+    
+    logger.debug("Initializing CdaExecutor");
+
+  }
+
+  public static void main(String[] args) {
+
+
+    final CdaExecutor cdaExecutor = CdaExecutor.getInstance();
+
+    cdaExecutor.doQuery();
+
+  }
+
+  private void doQuery() {
+
+    logger.info("Doing query on Cda");
+
+  }
+
+
+  private static CdaExecutor getInstance() {
+
+    if (_instance == null)
+      _instance = new CdaExecutor();
+
+    return _instance;
+  }
+
+}
