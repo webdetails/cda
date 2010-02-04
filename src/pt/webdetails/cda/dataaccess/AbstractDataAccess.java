c62dbc7121a03a19965e9941c6a35253d8c8a64b
torianus.lucanicus <torianus.lucanicus@55ec6a14-0f31-11df-9e5d-c9c78cbd7d0e>
2010-02-04 00:20:34 +0000

    Added the copyTableModel method to make it easy to create copies of existing data

--- src/pt/webdetails/cda/dataaccess/AbstractDataAccess.java
+++ src/pt/webdetails/cda/dataaccess/AbstractDataAccess.java
@@ -1,11 +1,12 @@
 package pt.webdetails.cda.dataaccess;
 
+import javax.swing.table.TableModel;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dom4j.Element;
+import org.pentaho.reporting.engine.classic.core.util.TypedTableModel;
 import pt.webdetails.cda.settings.CdaSettings;
-
-import javax.swing.table.TableModel;
 
 /**
  * This is the top level implementation of a DataAccess. Only the common methods are used here
@@ -14,7 +15,8 @@ import javax.swing.table.TableModel;
  * Date: Feb 3, 2010
  * Time: 11:05:38 AM
  */
-public abstract class AbstractDataAccess implements DataAccess {
+public abstract class AbstractDataAccess implements DataAccess
+{
 
   private static final Log logger = LogFactory.getLog(AbstractDataAccess.class);
 
@@ -25,23 +27,31 @@ public abstract class AbstractDataAccess implements DataAccess {
   private int cacheDuration = 3600;
 
 
-  protected AbstractDataAccess(final Element element) {
+  protected AbstractDataAccess(final Element element)
+  {
 
     id = element.attributeValue("id");
 
     if (element.attributeValue("access") != null && element.attributeValue("access").equals("private"))
+    {
       access = DataAccessEnums.ACCESS_TYPE.PRIVATE;
+    }
 
     if (element.attributeValue("cache") != null && element.attributeValue("cache").equals("true"))
+    {
       cache = true;
+    }
 
     if (element.attribute("cacheDuration") != null)
-       cacheDuration = Integer.parseInt(element.attributeValue("cacheDuration"));
+    {
+      cacheDuration = Integer.parseInt(element.attributeValue("cacheDuration"));
+    }
 
   }
 
   @Override
-  public String getId() {
+  public String getId()
+  {
     return id;
   }
 
@@ -49,24 +59,51 @@ public abstract class AbstractDataAccess implements DataAccess {
   public abstract TableModel queryData() throws QueryException;
 
 
-  public DataAccessEnums.ACCESS_TYPE getAccess() {
+  public DataAccessEnums.ACCESS_TYPE getAccess()
+  {
     return access;
   }
 
-  public boolean isCache() {
+  public boolean isCache()
+  {
     return cache;
   }
 
-  public int getCacheDuration() {
+  public int getCacheDuration()
+  {
     return cacheDuration;
   }
 
-  public CdaSettings getCdaSettings() {
+  public CdaSettings getCdaSettings()
+  {
     return cdaSettings;
   }
 
-  public void setCdaSettings(CdaSettings cdaSettings) {
+  public void setCdaSettings(CdaSettings cdaSettings)
+  {
     this.cdaSettings = cdaSettings;
   }
-  
+
+  protected static TableModel copyTableModel(TableModel t)
+  {
+    final int count = t.getColumnCount();
+    final Class[] colTypes = new Class[count];
+    final String[] colNames = new String[count];
+    for (int i = 0; i < count; i++)
+    {
+      colTypes[i] = t.getColumnClass(i);
+      colNames[i] = t.getColumnName(i);
+    }
+    final int rowCount = t.getRowCount();
+    final TypedTableModel typedTableModel = new TypedTableModel(colNames, colTypes, rowCount);
+    for (int r = 0; r < rowCount; r++)
+    {
+      for (int c = 0; c < count; c++)
+      {
+        typedTableModel.setValueAt(t.getValueAt(r, c), r, c);
+      }
+    }
+    return typedTableModel;
+  }
+
 }
