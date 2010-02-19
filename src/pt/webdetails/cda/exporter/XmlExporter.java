7c5608283b641c16e41750bb413061a5e3857e44
torianus.lucanicus <torianus.lucanicus@55ec6a14-0f31-11df-9e5d-c9c78cbd7d0e>
2010-02-19 15:19:46 +0000

    Bit of performance.

--- src/pt/webdetails/cda/exporter/XmlExporter.java
+++ src/pt/webdetails/cda/exporter/XmlExporter.java
@@ -32,7 +32,7 @@ public class XmlExporter extends AbstractExporter
   {
 
 
-    Document document = DocumentHelper.createDocument();
+    final Document document = DocumentHelper.createDocument();
 
     // Generate metadata
 
@@ -45,26 +45,27 @@ public class XmlExporter extends AbstractExporter
 
     for (int i = 0; i < columnCount; i++)
     {
-      Element columnInfo = metadata.addElement("ColumnMetaData");
-      columnInfo.addAttribute("index", (new Integer(i).toString()));
+      final Element columnInfo = metadata.addElement("ColumnMetaData");
+      columnInfo.addAttribute("index", (String.valueOf(i)));
       columnInfo.addAttribute("type", getColType(tableModel.getColumnClass(i)));
       columnInfo.addAttribute("name", tableModel.getColumnName(i));
 
     }
 
-    Element resultSet = root.addElement("ResultSet");
+    final Element resultSet = root.addElement("ResultSet");
     for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
     {
 
-      Element row = resultSet.addElement("Row");
+      final Element row = resultSet.addElement("Row");
 
       for (int colIdx = 0; colIdx < columnCount; colIdx++)
       {
         final Element col = row.addElement("Col");
-        
-        if (tableModel.getValueAt(rowIdx, colIdx) != null)
+
+        final Object value = tableModel.getValueAt(rowIdx, colIdx);
+        if (value != null)
         {
-          col.setText(tableModel.getValueAt(rowIdx, colIdx).toString());
+          col.setText(value.toString());
         }
         else{
           col.addAttribute("isNull","true");
@@ -75,7 +76,7 @@ public class XmlExporter extends AbstractExporter
 
     try
     {
-      Writer writer = new BufferedWriter(new OutputStreamWriter(out));
+      final Writer writer = new BufferedWriter(new OutputStreamWriter(out));
 
       document.setXMLEncoding("UTF-8");
       document.write(writer);
