289f85547ee675a9d362a605ca156bec76ed97e3
pmgalves <pmgalves@55ec6a14-0f31-11df-9e5d-c9c78cbd7d0e>
2010-03-10 18:21:54 +0000

    - fixed bugs in csv exporter

--- src/pt/webdetails/cda/exporter/CsvExporter.java
+++ src/pt/webdetails/cda/exporter/CsvExporter.java
@@ -1,13 +1,9 @@
 package pt.webdetails.cda.exporter;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import javax.swing.table.TableModel;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * JsonExporter
@@ -16,48 +12,66 @@ import org.json.JSONObject;
  * Date: Feb 5, 2010
  * Time: 5:07:12 PM
  */
-public class CsvExporter extends AbstractExporter {
+public class CsvExporter extends AbstractExporter
+{
 
   private String separator;
   public static final String newLine = System.getProperty("line.separator");
+  private DecimalFormat doubleFormatter = new DecimalFormat("##0.#######");
 
-  public CsvExporter() {
+  public CsvExporter()
+  {
     super();
     separator = ";";
   }
 
-  public CsvExporter(String separator) {
+  public CsvExporter(String separator)
+  {
     super();
     this.separator = separator;
   }
 
-  public void export(final OutputStream out, final TableModel tableModel) throws ExporterException {
+  public void export(final OutputStream out, final TableModel tableModel) throws ExporterException
+  {
 
 
     final int columnCount = tableModel.getColumnCount();
     final int rowCount = tableModel.getRowCount();
     PrintWriter pw = new PrintWriter(out);
 
-    for (int i = 0; i < columnCount; i++) {
+    for (int i = 0; i < columnCount; i++)
+    {
       pw.append(tableModel.getColumnName(i) + separator);
     }
     pw.append(newLine);
 
-    for (int rowIdx = 0; rowIdx < rowCount; rowIdx++) {
-      for (int colIdx = 0; colIdx < columnCount; colIdx++) {
-        String value = tableModel.getValueAt(rowIdx, colIdx).toString();
-        if (isDouble(value)) {
-          pw.append(new Double(value).toString());
-        } else {
+    for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
+    {
+      for (int colIdx = 0; colIdx < columnCount; colIdx++)
+      {
+        String value = tableModel.getValueAt(rowIdx, colIdx) == null ? null : tableModel.getValueAt(rowIdx, colIdx).toString();
+        if (value == null)
+        {
+          // nothing
+        }
+        else if (isDouble(value))
+        {
+          pw.append(doubleFormatter.format(Double.parseDouble(value)));
+        }
+        else
+        {
           String aux = value.replaceAll("\"", "\\\\\"");
           pw.append('\"' + aux + '\"');
         }
-        pw.append(separator);
+
+        if (colIdx != rowCount - 1)
+        {
+          pw.append(separator);
+        }
       }
       pw.append(newLine);
     }
     pw.flush();
-
 
 
   }
@@ -67,10 +81,14 @@ public class CsvExporter extends AbstractExporter {
     return "text/csv";
   }
 
-  public boolean isDouble(String obj) {
-    try {
+  public boolean isDouble(String obj)
+  {
+    try
+    {
       Double.parseDouble(obj);
-    } catch (NumberFormatException e) {
+    }
+    catch (NumberFormatException e)
+    {
       return false;
     }
     return true;
