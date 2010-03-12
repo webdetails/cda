eb38dbd9c04c062d671d8c3c374e405729d43e1c
pmgalves <pmgalves@55ec6a14-0f31-11df-9e5d-c9c78cbd7d0e>
2010-03-12 19:44:17 +0000

    - CsvExporter

--- src/pt/webdetails/cda/exporter/CsvExporter.java
+++ src/pt/webdetails/cda/exporter/CsvExporter.java
@@ -1,96 +1,92 @@
 package pt.webdetails.cda.exporter;
 
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.text.DecimalFormat;
-import javax.swing.table.TableModel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
- * JsonExporter
- * <p/>
- * User: pedro
- * Date: Feb 5, 2010
- * Time: 5:07:12 PM
+ * Created by IntelliJ IDEA. User: pedro Date: Feb 16, 2010 Time: 11:38:19 PM
  */
-public class CsvExporter extends AbstractExporter
+public class CsvExporter extends AbstractKettleExporter
 {
 
-  private String separator;
-  public static final String newLine = System.getProperty("line.separator");
-  private DecimalFormat doubleFormatter = new DecimalFormat("##0.#######");
+  private static final Log logger = LogFactory.getLog(CsvExporter.class);
+
 
   public CsvExporter()
   {
-    super();
-    separator = ";";
   }
 
-  public CsvExporter(String separator)
+  protected String getExportStepDefinition(final String name)
   {
-    super();
-    this.separator = separator;
-  }
-
-  public void export(final OutputStream out, final TableModel tableModel) throws ExporterException
-  {
+    StringBuilder xml = new StringBuilder();
 
 
-    final int columnCount = tableModel.getColumnCount();
-    final int rowCount = tableModel.getRowCount();
-    PrintWriter pw = new PrintWriter(out);
+    xml.append("<step>\n" +
+        "    <name>"+ name + "</name>\n" +
+        "    <type>TextFileOutput</type>\n" +
+        "    <description/>\n" +
+        "    <distribute>Y</distribute>\n" +
+        "    <copies>1</copies>\n" +
+        "         <partitioning>\n" +
+        "           <method>none</method>\n" +
+        "           <schema_name/>\n" +
+        "           </partitioning>\n" +
+        "    <separator>;</separator>\n" +
+        "    <enclosure>&quot;</enclosure>\n" +
+        "    <enclosure_forced>N</enclosure_forced>\n" +
+        "    <header>Y</header>\n" +
+        "    <footer>N</footer>\n" +
+        "    <format>DOS</format>\n" +
+        "    <compression>None</compression>\n" +
+        "    <encoding/>\n" +
+        "    <endedLine/>\n" +
+        "    <fileNameInField>N</fileNameInField>\n" +
+        "    <fileNameField/>\n" +
+        "    <file>\n" +
+        "      <name>${java.io.tmpdir}&#47;");
 
-    for (int i = 0; i < columnCount; i++)
-    {
-      pw.append(tableModel.getColumnName(i) + separator);
-    }
-    pw.append(newLine);
+    xml.append(getFileName());
 
-    for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
-    {
-      for (int colIdx = 0; colIdx < columnCount; colIdx++)
-      {
-        String value = tableModel.getValueAt(rowIdx, colIdx) == null ? null : tableModel.getValueAt(rowIdx, colIdx).toString();
-        if (value == null)
-        {
-          // nothing
-        }
-        else if (isDouble(value))
-        {
-          pw.append(doubleFormatter.format(Double.parseDouble(value)));
-        }
-        else
-        {
-          String aux = value.replaceAll("\"", "\\\\\"");
-          pw.append('\"' + aux + '\"');
-        }
 
-        if (colIdx != rowCount - 1)
-        {
-          pw.append(separator);
-        }
-      }
-      pw.append(newLine);
-    }
-    pw.flush();
+    xml.append("</name>\n" +
+        "      <is_command>N</is_command>\n" +
+        "      <do_not_open_new_file_init>N</do_not_open_new_file_init>\n" +
+        "      <extention>csv</extention>\n" +
+        "      <append>N</append>\n" +
+        "      <split>N</split>\n" +
+        "      <haspartno>N</haspartno>\n" +
+        "      <add_date>N</add_date>\n" +
+        "      <add_time>N</add_time>\n" +
+        "      <SpecifyFormat>N</SpecifyFormat>\n" +
+        "      <date_time_format/>\n" +
+        "      <add_to_result_filenames>Y</add_to_result_filenames>\n" +
+        "      <pad>N</pad>\n" +
+        "      <fast_dump>N</fast_dump>\n" +
+        "      <splitevery>0</splitevery>\n" +
+        "    </file>\n" +
+        "    <fields>\n" +
+        "    </fields>\n" +
+        "     <cluster_schema/>\n" +
+        " <remotesteps>   <input>   </input>   <output>   </output> </remotesteps>    <GUI>\n" +
+        "      <xloc>474</xloc>\n" +
+        "      <yloc>139</yloc>\n" +
+        "      <draw>Y</draw>\n" +
+        "      </GUI>\n" +
+        "    </step>");
 
+    return xml.toString();
 
   }
 
   public String getMimeType()
   {
-    return "text/csv";
+    return "application/vnd.ms-excel";
   }
 
-  public boolean isDouble(String obj)
+  public String getType()
   {
-    try
-    {
-      Double.parseDouble(obj);
-    }
-    catch (NumberFormatException e)
-    {
-      return false;
-    }
-    return true;
+    return "csv";
   }
+
+
 }
