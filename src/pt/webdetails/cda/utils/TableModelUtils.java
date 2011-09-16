faa0b4d41f928e65050352ecca3251e27b36cc41
pdpinheiro@gmail.com <pdpinheiro@gmail.com@55ec6a14-0f31-11df-9e5d-c9c78cbd7d0e>
2011-09-16 15:59:27 +0000

    preemptively return empty table models when post processing

--- src/pt/webdetails/cda/utils/TableModelUtils.java
+++ src/pt/webdetails/cda/utils/TableModelUtils.java
@@ -42,6 +42,8 @@ public class TableModelUtils
     
     if(rawTableModel == null){
       throw new IllegalArgumentException("Cannot process null table.");
+    } else if (rawTableModel.getRowCount() == 0) {
+      return rawTableModel;
     }
 
     // We will:
