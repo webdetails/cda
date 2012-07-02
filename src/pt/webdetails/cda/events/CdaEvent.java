8f0d5e7208dd207289dc3486a5be1a6397c934ad
Pedro Alves <pedro.alves@webdetails.pt>
2012-07-02 17:18:11 +0100

    - Plugin event now sends params as array of arrays instead of objects

--- src/pt/webdetails/cda/events/CdaEvent.java
+++ src/pt/webdetails/cda/events/CdaEvent.java
@@ -1,9 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 package pt.webdetails.cda.events;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.pentaho.reporting.engine.classic.core.ParameterDataRow;
@@ -11,76 +11,93 @@ import org.pentaho.reporting.engine.classic.core.ParameterDataRow;
 import pt.webdetails.cpf.JsonSerializable;
 import pt.webdetails.cpf.messaging.PluginEvent;
 
-public abstract class CdaEvent extends PluginEvent {
-  
-  public static class Fields extends PluginEvent.Fields {
+public abstract class CdaEvent extends PluginEvent
+{
+
+  public static class Fields extends PluginEvent.Fields
+  {
+
     public static String QUERY_INFO = "queryInfo";
   }
-  
-  enum CdaEventType {
+
+  enum CdaEventType
+  {
+
     QueryTooLong,
-    QueryError,
-  }
-  
-  public static class QueryInfo implements JsonSerializable {
+    QueryError,}
+
+  public static class QueryInfo implements JsonSerializable
+  {
+
     String cdaSettingsId;
     String dataAccessId;
     String query;
     ParameterDataRow parameters;
-    
-    public QueryInfo(String cdaSettingsId, String dataAccessId, String query, ParameterDataRow parameters){
+
+
+    public QueryInfo(String cdaSettingsId, String dataAccessId, String query, ParameterDataRow parameters)
+    {
       this.cdaSettingsId = cdaSettingsId;
       this.dataAccessId = dataAccessId;
       this.query = query;
       this.parameters = parameters;
     }
 
+
     @Override
-    public JSONObject toJSON() throws JSONException {
+    public JSONObject toJSON() throws JSONException
+    {
       JSONObject obj = new JSONObject();
       obj.put("cdaSettingsId", cdaSettingsId);
       obj.put("dataAccessId", dataAccessId);
       obj.put("query", query);
-      
-      JSONObject params = new JSONObject();
-      for(String paramName : parameters.getColumnNames()){
-        params.put(paramName,parameters.get(paramName));
+
+      JSONArray params = new JSONArray();
+      for (String paramName : parameters.getColumnNames())
+      {
+        JSONArray param = new JSONArray();
+        param.put( 0, (Object) paramName ).put( 1, (Object) parameters.get(paramName));
+        params.put(param);
       }
       obj.put("parameters", params);
-      
+
       return obj;
     }
-
   }
-  
   private QueryInfo queryInfo;
 
-  public CdaEvent(CdaEventType eventType, QueryInfo queryInfo) throws JSONException{
-    super("cda",eventType.toString(), queryInfo.cdaSettingsId);
+
+  public CdaEvent(CdaEventType eventType, QueryInfo queryInfo) throws JSONException
+  {
+    super("cda", eventType.toString(), queryInfo.cdaSettingsId);
     this.queryInfo = queryInfo;
     JSONObject key = queryInfo.toJSON();
     key.put(Fields.EVENT_TYPE, eventType.toString());
     setKey(key);
   }
-  
+
 //  public CdaEvent(JSONObject obj) throws JSONException {
 //    super(obj);
 //    this.queryInfo = new QueryInfo(obj.getJSONObject(Fields.QUERY_INFO));
 //  }
 
-  public QueryInfo getQueryInfo() {
+  public QueryInfo getQueryInfo()
+  {
     return queryInfo;
   }
 
-  public void setQueryInfo(QueryInfo queryInfo) {
+
+  public void setQueryInfo(QueryInfo queryInfo)
+  {
     this.queryInfo = queryInfo;
   }
-  
-  @Override 
-  public JSONObject toJSON() throws JSONException{
+
+
+  @Override
+  public JSONObject toJSON() throws JSONException
+  {
     JSONObject obj = super.toJSON();
     obj.put(Fields.QUERY_INFO, queryInfo.toJSON());
     return obj;
   }
-
 }
