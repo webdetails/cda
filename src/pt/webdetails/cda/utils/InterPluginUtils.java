18b837debddc13f45193665dcd154fa73b563398
Pedro Alves <pedro.alves@webdetails.pt>
2012-07-03 16:55:36 +0100

    - Added ability to pass params to interplugin calls

--- src/pt/webdetails/cda/utils/InterPluginUtils.java
+++ src/pt/webdetails/cda/utils/InterPluginUtils.java
@@ -5,6 +5,7 @@
 package pt.webdetails.cda.utils;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,19 +38,25 @@ public class InterPluginUtils
     return _instance;
   }
 
-  
-  
-  public static TableModel getTableModelFromJsonPluginCall(String plugin, String method){
-    
-    
-    InterPluginCall pluginCall = new InterPluginCall(new InterPluginCall.Plugin(plugin), method);
-    
-    return InterPluginUtils.getInstance().getTableModelFromJSONArray(pluginCall.call());
-    
-  }
-  
 
-  public TableModel getTableModelFromJSONArray(String json)
+  public static TableModel getTableModelFromJsonPluginCall(String plugin, String method)
+  {
+    return getTableModelFromJsonPluginCall(plugin, method, new HashMap<String, Object>());
+  }
+
+
+  public static TableModel getTableModelFromJsonPluginCall(String plugin, String method, Map<String, Object> params)
+  {
+
+
+    InterPluginCall pluginCall = new InterPluginCall(new InterPluginCall.Plugin(plugin), method, params);
+
+    return InterPluginUtils.getInstance().getTableModelFromJSONArray(pluginCall.call(), params);
+
+  }
+
+
+  public TableModel getTableModelFromJSONArray(String json, Map<String, Object> params)
   {
 
 
@@ -72,25 +79,55 @@ public class InterPluginUtils
         }
       }
 
-      TypedTableModel tm = new TypedTableModel(cols.keySet().toArray(new String[0]), cols.values().toArray(new Class[0]), list.size());
-      logger.debug("Done. cols has " + cols.size() + " entries");
+      // Do we have columns parameter? If so, add that
+      Map<Object, Class> columnOutputs;
+      if (params != null && params.containsKey("columns"))
+      {
+        String[] columns = (String[]) params.get("columns");
+        columnOutputs = new LinkedHashMap<Object, Class>();
+        for (int i = 0; i < columns.length; i++)
+        {
+          String column = columns[i];
+          // Do we have this in our mapping?
+          if (cols.containsKey(column))
+          {
+            columnOutputs.put(column, cols.get(column));
+          }
+          else
+          {
+            columnOutputs.put(column, String.class);
+          }
+        }
+
+
+      }
+      else
+      {
+        columnOutputs = cols;
+      }
+
+
+
+      TypedTableModel tm = new TypedTableModel(columnOutputs.keySet().toArray(new String[0]), columnOutputs.values().toArray(new Class[0]), list.size());
+      logger.debug("Done. columnOutputs has " + columnOutputs.size() + " entries");
 
       // Fill the table model
       for (int i = 0; i < list.size(); i++)
       {
         Map map = list.get(i);
-        
-        // Iterate through the cols
-        Object[] keyset = cols.keySet().toArray();
-        for (int j = 0; j < keyset.length ; j++)
+
+        // Iterate through the columnOutputs
+        Object[] keyset = columnOutputs.keySet().toArray();
+        for (int j = 0; j < keyset.length; j++)
         {
           Object key = keyset[j];
-          if(map.containsKey(key)){
+          if (map.containsKey(key))
+          {
             tm.setValueAt(map.get(key), i, j);
           }
-          
+
         }
-        
+
       }
 
 
@@ -114,7 +151,19 @@ public class InterPluginUtils
 
     System.out.println("TEST");
 
-    InterPluginUtils.getInstance().getTableModelFromJSONArray("[{\"id\":1,\"type\":\"query\",\"name\":\"Test 1\",\"group\":\"CDV Sample Tests\",\"createdBy\":\"Pedro\",\"createdAt\":1339430893246,\"validation\":\"/plugin-samples/cda/cdafiles/sql-jndi.cda[1] \\n/plugin-samples/cda/cdafiles/sql-jndi.cda[1] (status: Cancelled)\",\"validationName\":\"Test Existence\",\"validationType\":\"custom\",\"expected\":100,\"warnPercentage\":0.3,\"errorPercentage\":0.7,\"errorOnLow\":true,\"cron\":\"0 2 * * ? *\"},{\"id\":2,\"type\":\"query\",\"name\":\"Test 2\",\"group\":\"CDV Sample Tests\",\"createdBy\":\"Pedro\",\"createdAt\":1339430893246,\"validation\":\"/plugin-samples/cda/cdafiles/mondrian-jndi.cda[1] \\n/plugin-samples/cda/cdafiles/mondrian-jndi.cda[1] (status: Cancelled)\",\"validationName\":\"Test Existence\",\"validationType\":\"custom\",\"expected\":100,\"warnPercentage\":0.3,\"errorPercentage\":0.7,\"errorOnLow\":true,\"cron\":\"0 2 * * ? *\"}]");
+
+    TableModel tm1 = InterPluginUtils.getInstance().getTableModelFromJSONArray("[{\"id\":1,\"type\":\"query\",\"name\":\"Test 1\",\"group\":\"CDV Sample Tests\",\"createdBy\":\"Pedro\",\"createdAt\":1339430893246,\"validation\":\"/plugin-samples/cda/cdafiles/sql-jndi.cda[1] \\n/plugin-samples/cda/cdafiles/sql-jndi.cda[1] (status: Cancelled)\",\"validationName\":\"Test Existence\",\"validationType\":\"custom\",\"expected\":100,\"warnPercentage\":0.3,\"errorPercentage\":0.7,\"errorOnLow\":true,\"cron\":\"0 2 * * ? *\"},{\"id\":2,\"type\":\"query\",\"name\":\"Test 2\",\"group\":\"CDV Sample Tests\",\"createdBy\":\"Pedro\",\"createdAt\":1339430893246,\"validation\":\"/plugin-samples/cda/cdafiles/mondrian-jndi.cda[1] \\n/plugin-samples/cda/cdafiles/mondrian-jndi.cda[1] (status: Cancelled)\",\"validationName\":\"Test Existence\",\"validationType\":\"custom\",\"expected\":100,\"warnPercentage\":0.3,\"errorPercentage\":0.7,\"errorOnLow\":true,\"cron\":\"0 2 * * ? *\"}]", null);
+
+    HashMap params = new HashMap<String, Object>();
+    params.put("columns", new String[]
+            {
+//              "timestamp", "file", "parameters", "message"
+              "id", "XXX","group"
+            });
+
+    TableModel tm2 = InterPluginUtils.getInstance().getTableModelFromJSONArray("[{\"id\":1,\"type\":\"query\",\"name\":\"Test 1\",\"group\":\"CDV Sample Tests\",\"createdBy\":\"Pedro\",\"createdAt\":1339430893246,\"validation\":\"/plugin-samples/cda/cdafiles/sql-jndi.cda[1] \\n/plugin-samples/cda/cdafiles/sql-jndi.cda[1] (status: Cancelled)\",\"validationName\":\"Test Existence\",\"validationType\":\"custom\",\"expected\":100,\"warnPercentage\":0.3,\"errorPercentage\":0.7,\"errorOnLow\":true,\"cron\":\"0 2 * * ? *\"},{\"id\":2,\"type\":\"query\",\"name\":\"Test 2\",\"group\":\"CDV Sample Tests\",\"createdBy\":\"Pedro\",\"createdAt\":1339430893246,\"validation\":\"/plugin-samples/cda/cdafiles/mondrian-jndi.cda[1] \\n/plugin-samples/cda/cdafiles/mondrian-jndi.cda[1] (status: Cancelled)\",\"validationName\":\"Test Existence\",\"validationType\":\"custom\",\"expected\":100,\"warnPercentage\":0.3,\"errorPercentage\":0.7,\"errorOnLow\":true,\"cron\":\"0 2 * * ? *\"}]", params);
+
+
 
   }
 }
