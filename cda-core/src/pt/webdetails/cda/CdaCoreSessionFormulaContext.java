cc676e4de3e35db43c8ede254a8a4a4d0c37cd9b
Joao Velasques <joao.velasques@webdetails.pt>
2013-04-04 12:35:57 +0100

    Refactoring in progress

---
+++ cda-core/src/pt/webdetails/cda/CdaCoreSessionFormulaContext.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.webdetails.cda;
+
+/**
+ *
+ * @author joao
+ */
+public class CdaCoreSessionFormulaContext implements ICdaCoreSessionFormulaContext {
+    
+}
