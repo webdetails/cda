07b03a005ff40363cbabade9914cba895f4d6c47
Joao Velasques <joao.velasques@webdetails.pt>
2013-04-08 14:39:55 +0100

    Not relevant code changes, just commentary on most classes

--- cda-core/src/pt/webdetails/cda/utils/mondrian/CompactBandedMDXDataFactory.java
+++ cda-core/src/pt/webdetails/cda/utils/mondrian/CompactBandedMDXDataFactory.java
@@ -1,19 +1,6 @@
-/*
- * This program is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- * Foundation.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- * or from the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * Copyright (c) 2008 - 2009 Pentaho Corporation, .  All rights reserved.
- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package pt.webdetails.cda.utils.mondrian;
 
