b6db50c5100f4bae0595d9a94a372b2e0e2f5c16
Joao Velasques <joao.velasques@webdetails.pt>
2013-04-17 19:11:10 +0100

    Starting the CdaCoreService tests

--- cda-core/test-src/pt/webdetails/cda/tests/CompoundJoinTest.java
+++ cda-core/test-src/pt/webdetails/cda/tests/CompoundJoinTest.java
@@ -4,12 +4,20 @@ import java.io.File;
 import java.io.OutputStream;
 import java.net.URL;
 
-import junit.framework.TestCase;
+import net.sf.ehcache.CacheManager;
+
+
+import org.junit.Assert;
+import org.junit.Test;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.junit.BeforeClass;
 import pt.webdetails.cda.CdaEngine;
+import pt.webdetails.cda.CoreBeanFactory;
+import pt.webdetails.cda.DefaultCdaEnvironment;
+import pt.webdetails.cda.InitializationException;
 import pt.webdetails.cda.query.QueryOptions;
 import pt.webdetails.cda.settings.CdaSettings;
 import pt.webdetails.cda.settings.SettingsManager;
@@ -20,25 +28,36 @@ import pt.webdetails.cda.settings.SettingsManager;
  * Date: Feb 15, 2010
  * Time: 7:53:13 PM
  */
-public class CompoundJoinTest extends TestCase
+public class CompoundJoinTest
 {
 
   private static final Log logger = LogFactory.getLog(CompoundJoinTest.class);
 
+  @BeforeClass
+  public static void setUp() throws InitializationException{
+      CoreBeanFactory cbf = new CoreBeanFactory("cda.standalone.spring.xml");
+      DefaultCdaEnvironment env = new DefaultCdaEnvironment(cbf);
+      CdaEngine.init(env);
+  }
+  
+  @Test
   public void testCompoundQuery() throws Exception
   {
-
-
+try{
     // Define an outputStream
     OutputStream out = System.out;
 
     logger.info("Building CDA settings from sample file");
     final SettingsManager settingsManager = SettingsManager.getInstance();
-
+   // String fileString = CdaEngine.getEnvironment().getRepositoryAccess().getResourceAsString("sample-join.cda");
+    
+   // Assert.assertNotNull(fileString);
     
     URL file = this.getClass().getResource("sample-join.cda");
+    //Assert.assertNotNull(file);//could get file  XXX currently failing
+    
     File settingsFile = new File(file.toURI());
-    final CdaEngine engine = CdaEngine.getInstance();
+    
     final CdaSettings cdaSettings = settingsManager.parseSettingsFile(settingsFile.getAbsolutePath());
     logger.error("Doing query on Cda - Initializing CdaEngine");
     
@@ -46,10 +65,14 @@ public class CompoundJoinTest extends TestCase
     QueryOptions queryOptions = new QueryOptions();
     queryOptions.setDataAccessId("3");
     queryOptions.setOutputType("json");
-    // queryOptions.addParameter("status","In Process");
-
+    //queryOptions.addParameter("status","In Process");
     logger.info("Doing query");
-    engine.doQuery(out, cdaSettings, queryOptions);
+
+    CdaEngine.getInstance().doQuery(out, cdaSettings, queryOptions);
+}catch(Exception e){
+    e.printStackTrace();
+    Assert.fail();
+}
 
 
   }
