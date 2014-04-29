bcf60806115c4a18eed9db14f7cb5c222442082b
Pedro Vale <pedro.vale@webdetails.pt>
2014-04-30 00:29:51 +0100

    [CDA-76] - Abort in transformations results in a CDA error

--- cda-pentaho/src/pt/webdetails/cda/dataaccess/CdaPentahoKettleTransFromFileProducer.java
+++ cda-pentaho/src/pt/webdetails/cda/dataaccess/CdaPentahoKettleTransFromFileProducer.java
@@ -16,7 +16,6 @@ import org.pentaho.reporting.engine.classic.core.ParameterMapping;
 import org.pentaho.reporting.engine.classic.extensions.datasources.kettle.KettleTransFromFileProducer;
 import org.pentaho.reporting.libraries.resourceloader.ResourceKey;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
-import org.pentaho.reporting.platform.plugin.connection.PentahoKettleTransFromFileProducer;
 import org.pentaho.reporting.platform.plugin.RepositoryResourceLoader;
 
 public class CdaPentahoKettleTransFromFileProducer extends KettleTransFromFileProducer {
@@ -26,36 +25,30 @@ public class CdaPentahoKettleTransFromFileProducer extends KettleTransFromFilePr
                                                 final String username,
                                                 final String password,
                                                 final String[] definedArgumentNames,
-                                                final ParameterMapping[] definedVariableNames)
-  {
+                                                final ParameterMapping[] definedVariableNames ) {
 
     super( repositoryName, transformationFile, stepName, username, password, definedArgumentNames,
       definedVariableNames );
   }
 
   @Override
-  protected String computeFullFilename( ResourceKey key )
-  {
-    while ( key != null )
-    {
+  protected String computeFullFilename( ResourceKey key ) {
+    while ( key != null ) {
       final Object schema = key.getSchema();
       if ( RepositoryResourceLoader.SOLUTION_SCHEMA_NAME.equals( schema ) == false
-        && "pt.webdetails.cda.settings.CdaRepositoryResourceLoader:".equals( schema ) == false )
-      {
+        && "pt.webdetails.cda.settings.CdaRepositoryResourceLoader:".equals( schema ) == false ) {
         // these are not the droids you are looking for ..
         key = key.getParent();
         continue;
       }
 
       final Object identifier = key.getIdentifier();
-      if ( identifier instanceof String )
-      {
+      if ( identifier instanceof String ) {
         // get a local file reference ...
         final String file = (String) identifier;
         // pedro alves - Getting the file through normal apis
         final String fileName = PentahoSystem.getApplicationContext().getSolutionPath( file );
-        if ( fileName != null )
-        {
+        if ( fileName != null ) {
           return fileName;
         }
       }
