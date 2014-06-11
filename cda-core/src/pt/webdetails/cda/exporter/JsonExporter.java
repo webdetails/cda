6ca380477279f1e0c5129940ea147eb573be5699
Diogo Mariano <diogo.mariano@webdetails.pt>
2014-06-11 16:42:08 +0100

    [CDE-262] - Fixed cdv and xml export and added the functionality to download json and xml files if an attachment name is provided. Check style applied

--- cda-core/src/pt/webdetails/cda/exporter/JsonExporter.java
+++ cda-core/src/pt/webdetails/cda/exporter/JsonExporter.java
@@ -19,6 +19,8 @@ import java.util.Map;
 
 import javax.swing.table.TableModel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,67 +32,58 @@ import pt.webdetails.cda.utils.MetadataTableModel;
  * <p/>
  * User: pedro Date: Feb 5, 2010 Time: 5:07:12 PM
  */
-public class JsonExporter extends AbstractExporter
-{
-
+public class JsonExporter extends AbstractExporter {
+  private static final String MIME_TYPE = "application/json";
+  private static final Log logger = LogFactory.getLog( XmlExporter.class );
+  private String attachmentName;
   boolean isJsonp = false;
 
-
-  public JsonExporter()
-  {
+  public JsonExporter() {
     super();
   }
 
-
-  public JsonExporter(Map<String, String> extraSettings)
-  {
-    super(extraSettings);
-    if (this.getSetting("callback", null) != null)
-    {
+  public JsonExporter( Map<String, String> extraSettings ) {
+    super( extraSettings );
+    if ( this.getSetting( "callback", null ) != null ) {
       isJsonp = true;
     }
+    // the default should be null and if an attachment name is not given as attribute then the file should be
+    // returned inline
+    this.attachmentName = getSetting( ATTACHMENT_NAME_SETTING, null );
+
+    logger.debug( "Initialized JsonExporter with attachement filename '" + attachmentName + "'" );
   }
 
+  public void export( final OutputStream out, final TableModel tableModel ) throws ExporterException {
 
-  public void export(final OutputStream out, final TableModel tableModel) throws ExporterException
-  {
+    try {
+      JSONObject json = getTableAsJson( tableModel, null );
 
-    try
-    {
-      JSONObject json = getTableAsJson(tableModel, null);
+      try {
 
-      try
-      {
-
-        if (isJsonp)
-        {
-          out.write(this.getSetting("callback", "xxx").concat("(").getBytes("UTF-8"));
+        if ( isJsonp ) {
+          out.write( this.getSetting( "callback", "xxx" ).concat( "(" ).getBytes( "UTF-8" ) );
         }
 
-        out.write(json.toString().getBytes("UTF-8"));
+        out.write( json.toString().getBytes( "UTF-8" ) );
 
-        if (isJsonp)
-        {
-          out.write(");".getBytes("UTF-8"));
+        if ( isJsonp ) {
+          out.write( ");".getBytes( "UTF-8" ) );
         }
-      }
-      catch (IOException e)
-      {
-        throw new ExporterException("IO Exception converting to utf-8", e);
+        out.flush();
+      } catch ( IOException e ) {
+        throw new ExporterException( "IO Exception converting to utf-8", e );
       }
 
-    }
-    catch (JSONException e)
-    {
-      throw new ExporterException("JSONException building object", e);
+    } catch ( JSONException e ) {
+      throw new ExporterException( "JSONException building object", e );
     }
 
 
   }
 
 
-  public JSONObject getTableAsJson(TableModel tableModel, Integer rowLimit) throws JSONException, ExporterException
-  {
+  public JSONObject getTableAsJson( TableModel tableModel, Integer rowLimit ) throws JSONException, ExporterException {
     JSONObject json = new JSONObject();
 
     // Generate metadata
@@ -99,65 +92,52 @@ public class JsonExporter extends AbstractExporter
     final int columnCount = tableModel.getColumnCount();
     int rowCount = tableModel.getRowCount();
 
-    if (rowLimit != null)
-    {
-      rowCount = Math.min(rowCount, rowLimit);
+    if ( rowLimit != null ) {
+      rowCount = Math.min( rowCount, rowLimit );
     }
 
-    boolean[] isColumnDouble = new boolean[columnCount];
-    for (int i = 0; i < columnCount; i++)
-    {
+    boolean[] isColumnDouble = new boolean[ columnCount ];
+    for ( int i = 0; i < columnCount; i++ ) {
       JSONObject info = new JSONObject();
-      info.put("colIndex", i);
-      info.put("colName", tableModel.getColumnName(i));
-      Class<?> columnClass = tableModel.getColumnClass(i);
-      isColumnDouble[i] = (columnClass.isAssignableFrom(Double.class));
-      info.put("colType", getColType(columnClass));
-      metadataArray.put(info);
+      info.put( "colIndex", i );
+      info.put( "colName", tableModel.getColumnName( i ) );
+      Class<?> columnClass = tableModel.getColumnClass( i );
+      isColumnDouble[ i ] = ( columnClass.isAssignableFrom( Double.class ) );
+      info.put( "colType", getColType( columnClass ) );
+      metadataArray.put( info );
     }
-    json.put("metadata", metadataArray);
+    json.put( "metadata", metadataArray );
 
-    if (tableModel instanceof MetadataTableModel)
-    {
-      json.put("queryInfo", ((MetadataTableModel) tableModel).getAllMetadata());
+    if ( tableModel instanceof MetadataTableModel ) {
+      json.put( "queryInfo", ( (MetadataTableModel) tableModel ).getAllMetadata() );
     }
     final JSONArray valuesArray = new JSONArray();
 
-    for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
-    {
+    for ( int rowIdx = 0; rowIdx < rowCount; rowIdx++ ) {
       final JSONArray rowArray = new JSONArray();
-      for (int colIdx = 0; colIdx < columnCount; colIdx++)
-      {
-        Object value = tableModel.getValueAt(rowIdx, colIdx);
-        try
-        {
-          if (value != null && isColumnDouble[colIdx] && ((Double) value).isInfinite())
-          {
+      for ( int colIdx = 0; colIdx < columnCount; colIdx++ ) {
+        Object value = tableModel.getValueAt( rowIdx, colIdx );
+        try {
+          if ( value != null && isColumnDouble[ colIdx ] && ( (Double) value ).isInfinite() ) {
             value = null;
-            //value = Double.POSITIVE_INFINITY == (Double) value ? "Infinity" : "-Infinity";//workaround for JSON issue with Infinity
+            //value = Double.POSITIVE_INFINITY == (Double) value ? "Infinity" : "-Infinity";//workaround for JSON
+            // issue with Infinity
           }
-        }
-        catch (ClassCastException e)
-        {
+        } catch ( ClassCastException e ) {
         }//just because it says Double doesn't mean we don't get oranges
-        rowArray.put(value);
+        rowArray.put( value );
       }
-      valuesArray.put(rowArray);
+      valuesArray.put( rowArray );
     }
-    json.put("resultset", valuesArray);
+    json.put( "resultset", valuesArray );
     return json;
   }
 
-
-  public String getMimeType()
-  {
-    return "application/json";
+  public String getMimeType() {
+    return MIME_TYPE;
   }
 
-
-  public String getAttachmentName()
-  {
-    // No attachment required
-    return null;
+  public String getAttachmentName() {
+    return attachmentName;
   }
 }
--- cda-core/src/pt/webdetails/cda/exporter/XmlExporter.java
+++ cda-core/src/pt/webdetails/cda/exporter/XmlExporter.java
@@ -25,6 +25,8 @@ import java.util.Map;
 
 import javax.swing.table.TableModel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -33,94 +35,80 @@ import org.dom4j.Element;
 /**
  * XmlExporter
  * <p/>
- * User: pedro
- * Date: Feb 5, 2010
- * Time: 5:07:12 PM
+ * User: pedro Date: Feb 5, 2010 Time: 5:07:12 PM
  */
-public class XmlExporter extends AbstractExporter
-{
+public class XmlExporter extends AbstractExporter {
+  private static final String MIME_TYPE = "text/xml";
+  private static final Log logger = LogFactory.getLog( XmlExporter.class );
+  private String attachmentName;
 
-  public XmlExporter(Map <String,String> extraSettings)
-  {
-    super();
+  public XmlExporter( Map<String, String> extraSettings ) {
+    super( extraSettings );
+
+    // the default should be null and if an attachment name is not given as attribute then the file should be
+    // returned inline
+    this.attachmentName = getSetting( ATTACHMENT_NAME_SETTING, null );
+
+    logger.debug( "Initialized XmlExporter with attachement filename '" + attachmentName + "'" );
   }
 
 
-  public void export(final OutputStream out, final TableModel tableModel) throws ExporterException
-  {
-
-
+  public void export( final OutputStream out, final TableModel tableModel ) throws ExporterException {
     final Document document = DocumentHelper.createDocument();
 
     // Generate metadata
-
-    final Element root = document.addElement("CdaExport");
-
-    final Element metadata = root.addElement("MetaData");
+    final Element root = document.addElement( "CdaExport" );
+    final Element metadata = root.addElement( "MetaData" );
 
     final int columnCount = tableModel.getColumnCount();
     final int rowCount = tableModel.getRowCount();
 
-    for (int i = 0; i < columnCount; i++)
-    {
-      final Element columnInfo = metadata.addElement("ColumnMetaData");
-      columnInfo.addAttribute("index", (String.valueOf(i)));
-      columnInfo.addAttribute("type", getColType(tableModel.getColumnClass(i)));
-      columnInfo.addAttribute("name", tableModel.getColumnName(i));
+    for ( int i = 0; i < columnCount; i++ ) {
+      final Element columnInfo = metadata.addElement( "ColumnMetaData" );
+      columnInfo.addAttribute( "index", ( String.valueOf( i ) ) );
+      columnInfo.addAttribute( "type", getColType( tableModel.getColumnClass( i ) ) );
+      columnInfo.addAttribute( "name", tableModel.getColumnName( i ) );
 
     }
 
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
-    final Element resultSet = root.addElement("ResultSet");
-    for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
-    {
+    SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US );
+    final Element resultSet = root.addElement( "ResultSet" );
+    for ( int rowIdx = 0; rowIdx < rowCount; rowIdx++ ) {
 
-      final Element row = resultSet.addElement("Row");
+      final Element row = resultSet.addElement( "Row" );
 
-      for (int colIdx = 0; colIdx < columnCount; colIdx++)
-      {
-        final Element col = row.addElement("Col");
+      for ( int colIdx = 0; colIdx < columnCount; colIdx++ ) {
+        final Element col = row.addElement( "Col" );
 
-        final Object value = tableModel.getValueAt(rowIdx, colIdx);
-        if (value instanceof Date)
-        {
-          col.setText(format.format(value));
-        }
-        else if (value != null)
-        {
+        final Object value = tableModel.getValueAt( rowIdx, colIdx );
+        if ( value instanceof Date ) {
+          col.setText( format.format( value ) );
+        } else if ( value != null ) {
           // numbers can be safely converted via toString, as they use a well-defined format there
-          col.setText(value.toString());
-        }
-        else{
-          col.addAttribute("isNull","true");
+          col.setText( value.toString() );
+        } else {
+          col.addAttribute( "isNull", "true" );
         }
 
       }
     }
 
-    try
-    {
-      final Writer writer = new BufferedWriter(new OutputStreamWriter(out));
+    try {
+      final Writer writer = new BufferedWriter( new OutputStreamWriter( out ) );
 
-      document.setXMLEncoding("UTF-8");
-      document.write(writer);
+      document.setXMLEncoding( "UTF-8" );
+      document.write( writer );
       writer.flush();
+    } catch ( IOException e ) {
+      throw new ExporterException( "IO Exception converting to utf-8", e );
     }
-    catch (IOException e)
-    {
-      throw new ExporterException("IO Exception converting to utf-8", e);
-    }
-
   }
 
-  public String getMimeType()
-  {
-    return "text/xml";
+  public String getMimeType() {
+    return MIME_TYPE;
   }
 
-  public String getAttachmentName()
-  {
-    // No attachment required
-    return null;
+  public String getAttachmentName() {
+    return attachmentName;
   }
 }
