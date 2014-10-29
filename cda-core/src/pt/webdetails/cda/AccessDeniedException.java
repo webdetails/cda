f80ca15ff0c580d5bc891d9ca6a648395b8da315
Christian Wolff <cwolff@christians-imac.dusrz.de>
2014-10-29 16:11:24 +0100

    nix geaendert

--- cda-core/src/pt/webdetails/cda/AccessDeniedException.java
+++ cda-core/src/pt/webdetails/cda/AccessDeniedException.java
@@ -1,6 +1,6 @@
 /*!
 * Copyright 2002 - 2013 Webdetails, a Pentaho company.  All rights reserved.
-* 
+*
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
 * this file except in compliance with the license. If you need a copy of the license,
