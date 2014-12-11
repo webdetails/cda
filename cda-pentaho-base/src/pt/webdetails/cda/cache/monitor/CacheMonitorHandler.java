9bb98adddd54556bcba5f8f61231b243c13ebd80
David Santos <david.santos@webdetails.pt>
2014-12-11 18:45:53 +0000

    [CDA-106]: CDA Cache Manager - Operation refresh update 'Last Execution' field

--- cda-pentaho-base/src/pt/webdetails/cda/cache/monitor/CacheMonitorHandler.java
+++ cda-pentaho-base/src/pt/webdetails/cda/cache/monitor/CacheMonitorHandler.java
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2013 Webdetails, a Pentaho company.  All rights reserved.
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
 * 
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -30,17 +30,14 @@ import pt.webdetails.cda.utils.PentahoHelper;
 import pt.webdetails.cda.utils.framework.JsonCallHandler;
 
 //
-public class CacheMonitorHandler extends JsonCallHandler 
-{
-//  
-//  private static Log logger = LogFactory.getLog(JsonCallHandler.class);
-  
+public class CacheMonitorHandler extends JsonCallHandler {
+  //
+  //  private static Log logger = LogFactory.getLog(JsonCallHandler.class);
+
   private static CacheMonitorHandler _instance;
 
-  public static synchronized CacheMonitorHandler getInstance()
-  {
-    if (_instance == null)
-    {
+  public static synchronized CacheMonitorHandler getInstance() {
+    if ( _instance == null ) {
       _instance = new CacheMonitorHandler();
     }
     return _instance;
@@ -48,87 +45,80 @@ public class CacheMonitorHandler extends JsonCallHandler
 
   private CacheMonitor monitor;
 
-  public CacheMonitorHandler()
-  {
+  public CacheMonitorHandler() {
     this.monitor = new CacheMonitor();
     registerMethods();
   }
-  
 
-  protected boolean hasPermission(IPentahoSession session, Method method) 
-  {//limit all interaction besides overview to admin role
-    return method.getName().equals("cacheOverview") || PentahoHelper.isAdmin( session );
+
+  protected boolean hasPermission( IPentahoSession session,
+                                   Method method ) { //limit all interaction besides overview to admin role
+    return method.getName().equals( "cacheOverview" ) || PentahoHelper.isAdmin( session );
   }
-  
-  private void registerMethods()
-  {
-    
-    registerMethod("cached", new JsonCallHandler.Method() 
-    {
+
+  private void registerMethods() {
+
+    registerMethod( "cached", new JsonCallHandler.Method() {
       /**
        * get all cached items for given cda file and data access id
        */
-      public JSONObject execute(IParameterProvider params) throws JSONException, ExporterException, IOException {
-        String cdaSettingsId = params.getStringParameter("cdaSettingsId", null);
-        String dataAccessId = params.getStringParameter("dataAccessId", null);
-        return monitor.listQueriesInCache(cdaSettingsId, dataAccessId);
+      public JSONObject execute( IParameterProvider params ) throws JSONException, ExporterException, IOException {
+        String cdaSettingsId = params.getStringParameter( "cdaSettingsId", null );
+        String dataAccessId = params.getStringParameter( "dataAccessId", null );
+        return monitor.listQueriesInCache( cdaSettingsId, dataAccessId );
       }
-    });
-    
-    registerMethod("cacheOverview", new JsonCallHandler.Method() 
-    {
+    } );
+
+    registerMethod( "cacheOverview", new JsonCallHandler.Method() {
       /**
        * get details on a particular cached item
        */
-      public JSONObject execute(IParameterProvider params) throws JSONException 
-      {
-        String cdaSettingsId = params.getStringParameter("cdaSettingsId", null);
-        return monitor.getCachedQueriesOverview(cdaSettingsId);
+      public JSONObject execute( IParameterProvider params ) throws JSONException {
+        String cdaSettingsId = params.getStringParameter( "cdaSettingsId", null );
+        return monitor.getCachedQueriesOverview( cdaSettingsId );
       }
-    });
-    
-    registerMethod("getDetails", new JsonCallHandler.Method() 
-    {
+    } );
+
+    registerMethod( "getDetails", new JsonCallHandler.Method() {
       /**
        * get details on a particular cached item
        */
-      public JSONObject execute(IParameterProvider params) throws UnsupportedEncodingException, JSONException, ExporterException, IOException, ClassNotFoundException  
-      {
-          String encodedCacheKey=params.getStringParameter("key", null);
-          return monitor.getCacheQueryTable(encodedCacheKey);
+      public JSONObject execute( IParameterProvider params )
+        throws UnsupportedEncodingException, JSONException, ExporterException, IOException, ClassNotFoundException {
+        String encodedCacheKey = params.getStringParameter( "key", null );
+        return monitor.getCacheQueryTable( encodedCacheKey );
       }
-    });
-    
-    registerMethod("removeCache", new JsonCallHandler.Method() 
-    {
+    } );
+
+    registerMethod( "removeCache", new JsonCallHandler.Method() {
       /**
        * Remove item from cache 
        */
-      public JSONObject execute(IParameterProvider params) throws JSONException, UnsupportedEncodingException, IOException, ClassNotFoundException 
-      {
-        String serializedCacheKey = params.getStringParameter("key", null);
-        return monitor.removeQueryFromCache(serializedCacheKey);
+      public JSONObject execute( IParameterProvider params )
+        throws JSONException, UnsupportedEncodingException, IOException, ClassNotFoundException {
+        String serializedCacheKey = params.getStringParameter( "key", null );
+        return monitor.removeQueryFromCache( serializedCacheKey );
       }
-    });
-    
-    registerMethod("removeAll", new JsonCallHandler.Method() {
-      
+    } );
+
+    registerMethod( "removeAll", new JsonCallHandler.Method() {
+
       @Override
-      public JSONObject execute(IParameterProvider params) throws JSONException {
-        String cdaSettingsId = params.getStringParameter("cdaSettingsId", null);
-        String dataAccessId = params.getStringParameter("dataAccessId", null);
-        return monitor.removeAll(cdaSettingsId, dataAccessId);
+      public JSONObject execute( IParameterProvider params ) throws JSONException {
+        String cdaSettingsId = params.getStringParameter( "cdaSettingsId", null );
+        String dataAccessId = params.getStringParameter( "dataAccessId", null );
+        return monitor.removeAll( cdaSettingsId, dataAccessId );
       }
-    });
-    
-    registerMethod("shutdown", new JsonCallHandler.Method() {
-      
+    } );
+
+    registerMethod( "shutdown", new JsonCallHandler.Method() {
+
       @Override
-      public JSONObject execute(IParameterProvider params) throws Exception {
+      public JSONObject execute( IParameterProvider params ) throws Exception {
         //AbstractDataAccess.shutdownCache();
-        return monitor.shutdown();// getOKJson("Cache shutdown.");
+        return monitor.shutdown(); // getOKJson("Cache shutdown.");
       }
-    });
-    
+    } );
+
   }
 }
