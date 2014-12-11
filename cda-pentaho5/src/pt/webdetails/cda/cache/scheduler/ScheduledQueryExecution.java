ae2e920febde4de9ebdf6a865029e915891e6dd2
David Santos <david.santos@webdetails.pt>
2014-12-11 18:45:53 +0000

    [CDA-106]: CDA Cache Manager - Operation refresh update 'Last Execution' field

--- cda-pentaho5/src/pt/webdetails/cda/cache/scheduler/ScheduledQueryExecution.java
+++ cda-pentaho5/src/pt/webdetails/cda/cache/scheduler/ScheduledQueryExecution.java
@@ -1,15 +1,26 @@
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
+
 package pt.webdetails.cda.cache.scheduler;
 
 import org.pentaho.platform.api.scheduler2.Job;
 
 import pt.webdetails.cda.settings.SettingsManager;
 
-/**
- * 
- */
+
 public class ScheduledQueryExecution extends QueryExecution {
   public ScheduledQueryExecution( SettingsManager settingsMgr, String jsonString, Job job ) throws Exception {
-    super(settingsMgr, jsonString );
+    super( settingsMgr, jsonString );
     this.job = job;
   }
 
