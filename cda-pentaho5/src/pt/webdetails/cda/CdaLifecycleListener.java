da5d1a3315c1ab4f7f6e25e904db34759bf5df5a
Pedro Vale <pedro.vale@webdetails.pt>
2014-12-17 12:44:47 +0000

    [CLEANUP] - Changes in Reporting were causing compilation errors

--- cda-pentaho5/src/pt/webdetails/cda/CdaLifecycleListener.java
+++ cda-pentaho5/src/pt/webdetails/cda/CdaLifecycleListener.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IPluginLifecycleListener;
 import org.pentaho.platform.api.engine.PluginLifecycleException;
 
+import org.pentaho.reporting.engine.classic.core.metadata.MaturityLevel;
 import pt.webdetails.cda.utils.mondrian.CompactBandedMDXDataFactory;
 import pt.webdetails.cpf.PentahoPluginEnvironment;
 import pt.webdetails.cpf.PluginEnvironment;
@@ -17,49 +18,41 @@ import org.pentaho.reporting.engine.classic.core.metadata.DefaultDataFactoryCore
 
 /**
  * This class inits Cda plugin within the bi-platform
- * @author gorman
  *
+ * @author gorman
  */
-public class CdaLifecycleListener extends SimpleLifeCycleListener implements IPluginLifecycleListener
-{
+public class CdaLifecycleListener extends SimpleLifeCycleListener implements IPluginLifecycleListener {
 
-  static Log logger = LogFactory.getLog(CdaLifecycleListener.class);
+  static Log logger = LogFactory.getLog( CdaLifecycleListener.class );
 
 
-  public void init() throws PluginLifecycleException
-  {
+  public void init() throws PluginLifecycleException {
 
   }
 
 
-  public void loaded() throws PluginLifecycleException
-  {
+  public void loaded() throws PluginLifecycleException {
     ClassLoader contextCL = Thread.currentThread().getContextClassLoader();
-    try
-    {
+    try {
       super.loaded();
-      PluginEnvironment.init(CdaPluginEnvironment.getInstance());
-      CdaEngine.init(new PentahoCdaEnvironment());
+      PluginEnvironment.init( CdaPluginEnvironment.getInstance() );
+      CdaEngine.init( new PentahoCdaEnvironment() );
       CdaEngine.getInstance().getConfigProperty( "just load", null );
-  
+
       Class<CompactBandedMDXDataFactory> factoryClass = CompactBandedMDXDataFactory.class; // CHANGE ME!
-      DefaultDataFactoryMetaData dmd = new DefaultDataFactoryMetaData
-              (factoryClass.getName(), "", "", true, false, true, false, false, false, false, false, new DefaultDataFactoryCore(), 0);
-      DataFactoryRegistry.getInstance().register(dmd);
-    }
-    catch (Exception e)
-    {
-      logger.error("loading error", e);
-    }
-    finally
-    {
-      Thread.currentThread().setContextClassLoader(contextCL);
+      DefaultDataFactoryMetaData dmd = new DefaultDataFactoryMetaData(
+          factoryClass.getName(), "", "", true, false, true, false, false, false, false, MaturityLevel.Production,
+          new DefaultDataFactoryCore(), 0 );
+      DataFactoryRegistry.getInstance().register( dmd );
+    } catch ( Exception e ) {
+      logger.error( "loading error", e );
+    } finally {
+      Thread.currentThread().setContextClassLoader( contextCL );
     }
   }
 
 
-  public void unLoaded() throws PluginLifecycleException
-  {
+  public void unLoaded() throws PluginLifecycleException {
   }
 
   @Override
