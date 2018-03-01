2659dfab6bbdd85a1179e73f25c91ad4f1fa5194
Joao Figueiredo <joao.figueiredo@hitachivantara.com>
2018-03-01 12:42:49 +0000

    [BACKLOG-21553] data service name is not needed on the connection, since it is infered from the query

--- core/src/test/java/pt/webdetails/cda/connections/dataservices/DataservicesConnectionInfoTest.java
+++ core/src/test/java/pt/webdetails/cda/connections/dataservices/DataservicesConnectionInfoTest.java
@@ -13,15 +13,36 @@
 
 package pt.webdetails.cda.connections.dataservices;
 
+import org.dom4j.Document;
 import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.junit.Test;
 import org.mockito.Mockito;
 import pt.webdetails.cda.filetests.CdaTestCase;
-import java.util.Arrays;
-import static org.mockito.Mockito.*;
+import pt.webdetails.cpf.Util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DataservicesConnectionInfoTest extends CdaTestCase {
+
+  @Test
+  public void testConstructor() throws Exception {
+    Element connection = mock( Element.class );
+    when( connection.selectObject( Mockito.anyString() ) ).thenReturn( "" );
+
+    ArrayList<Element> properties = new ArrayList<>( );
+    properties.add( getElementFromSnippet( "<prop name=\"prop1\">prop value</prop>" ) );
+
+    when( connection.elements( anyString() ) ).thenReturn( properties );
+    DataservicesConnectionInfo dataservicesConnectionInfo = new DataservicesConnectionInfo( connection );
+    assertNotNull( dataservicesConnectionInfo.getProperties() );
+    assertEquals( 1, dataservicesConnectionInfo.getProperties().size() );
+  }
 
   @Test
   public void testHashCode() throws Exception {
@@ -29,6 +50,15 @@ public class DataservicesConnectionInfoTest extends CdaTestCase {
     when( connection.selectObject( Mockito.anyString() ) ).thenReturn( "" );
     DataservicesConnectionInfo dataservicesConnectionInfo = new DataservicesConnectionInfo( connection );
     assertEquals( 0, dataservicesConnectionInfo.hashCode() );
+  }
+
+  @Test
+  public void testGetProperties() throws Exception {
+    Element connection = mock( Element.class );
+    when( connection.selectObject( Mockito.anyString() ) ).thenReturn( "" );
+    DataservicesConnectionInfo dataservicesConnectionInfo = new DataservicesConnectionInfo( connection );
+    assertNotNull( dataservicesConnectionInfo.getProperties() );
+    assertEquals( 0, dataservicesConnectionInfo.getProperties().size() );
   }
 
   @Test
@@ -45,10 +75,15 @@ public class DataservicesConnectionInfoTest extends CdaTestCase {
     when( connection.getText() ).thenReturn( "someText" );
     when( otherConnection.elements( Mockito.anyString() ) ).thenReturn( Arrays.asList( connection ) );
     DataservicesConnectionInfo otherDataservicesConnectionInfo = new DataservicesConnectionInfo( otherConnection );
-    assertEquals( 2, otherDataservicesConnectionInfo.getProperties().size() );
-    assertEquals( "someStr", otherDataservicesConnectionInfo.getProperties().getProperty( "dataServiceName" ) );
+    assertEquals( 1, otherDataservicesConnectionInfo.getProperties().size() );
     assertEquals( "someText", otherDataservicesConnectionInfo.getProperties().getProperty( "someValue" ) );
-    assertFalse( dataservicesConnectionInfo.equals( otherDataservicesConnectionInfo ) );
+    assertTrue( dataservicesConnectionInfo.equals( otherDataservicesConnectionInfo ) );
     assertFalse( dataservicesConnectionInfo.equals( new Object() ) );
+  }
+
+  private Element getElementFromSnippet( String xml ) throws Exception {
+    SAXReader reader = new SAXReader( false );
+    Document doc = reader.read( Util.toInputStream( xml ) );
+    return doc.getRootElement();
   }
 }
--- core/src/test/java/pt/webdetails/cda/connections/dataservices/DataservicesConnectionTest.java
+++ core/src/test/java/pt/webdetails/cda/connections/dataservices/DataservicesConnectionTest.java
@@ -16,19 +16,78 @@ package pt.webdetails.cda.connections.dataservices;
 import org.dom4j.Element;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.pentaho.reporting.engine.classic.core.modules.misc.datafactory.sql.ConnectionProvider;
+import org.pentaho.reporting.engine.classic.core.modules.misc.datafactory.sql.DriverConnectionProvider;
+import pt.webdetails.cda.CdaEngine;
+import pt.webdetails.cda.connections.ConnectionCatalog;
+import pt.webdetails.cda.filetests.CdaTestEnvironment;
+import pt.webdetails.cda.filetests.CdaTestingContentAccessFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
-
 
 public class DataservicesConnectionTest {
 
   @Test
   public void testHashCode() throws Exception {
     DataservicesConnection dataservicesConnection = new DataservicesConnection();
-    assertEquals( dataservicesConnection.hashCode(), 0 );
+    assertEquals( 0, dataservicesConnection.hashCode() );
+  }
+
+  @Test
+  public void testGenericType() {
+    DataservicesConnection dataservicesConnection = new DataservicesConnection();
+    assertEquals( ConnectionCatalog.ConnectionType.DATASERVICES, dataservicesConnection.getGenericType() );
+  }
+
+  @Test
+  public void testGetType() {
+    DataservicesConnection dataservicesConnection = new DataservicesConnection();
+    assertEquals( DataservicesConnection.TYPE, dataservicesConnection.getType() );
+  }
+
+  @Test
+  public void testNewWithParameters() throws Exception {
+    Element connection = mock( Element.class );
+    when( connection.selectObject( Mockito.anyString() ) ).thenReturn( "" );
+    DataservicesConnection dataservicesConnection = new DataservicesConnection( connection );
+    assertNotNull( dataservicesConnection );
+  }
+
+  @Test
+  public void testGetInitializedConnectionProvider() throws Exception {
+    CdaTestingContentAccessFactory factory = new CdaTestingContentAccessFactory();
+    Connection connection = mock( Connection.class );
+    Statement statement = mock( Statement.class );
+    ResultSet resultSet = mock( ResultSet.class );
+    ResultSetMetaData resultSetMetaData = mock( ResultSetMetaData.class );
+    when( resultSet.next() ).thenReturn( true ).thenReturn( false );
+    when( resultSet.getMetaData() ).thenReturn( resultSetMetaData );
+    when( statement.executeQuery( Mockito.anyString() ) ).thenReturn( resultSet );
+    when( connection.createStatement( Mockito.anyInt(), Mockito.anyInt() ) ).thenReturn( statement );
+    DriverConnectionProvider dataserviceLocalConnectionProvider = mock( DriverConnectionProvider.class );
+    when( dataserviceLocalConnectionProvider.createConnection( Mockito.anyString(), Mockito.anyString() ) ).thenReturn( connection );
+    IDataservicesLocalConnection dataserviceLocalConnection = mock( IDataservicesLocalConnection.class );
+    when( dataserviceLocalConnection.getDriverConnectionProvider() ).thenReturn( dataserviceLocalConnectionProvider );
+    CdaTestEnvironment testEnvironment = spy( new CdaTestEnvironment( factory ) );
+    when( testEnvironment.getDataServicesLocalConnection() ).thenReturn( dataserviceLocalConnection );
+    CdaEngine.init( testEnvironment );
+
+    Element connectionElement = mock( Element.class );
+    when( connectionElement.selectObject( Mockito.anyString() ) ).thenReturn( "" );
+    DataservicesConnection dataservicesConnection = new DataservicesConnection( connectionElement );
+    ConnectionProvider connectionProvider = dataservicesConnection.getInitializedConnectionProvider();
+    assertNotNull( connectionProvider );
   }
 
   @Test
