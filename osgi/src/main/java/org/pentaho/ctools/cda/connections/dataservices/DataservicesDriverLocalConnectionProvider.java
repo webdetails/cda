059804988bfde0a116bcfde3dec43a9497a415b2
David Alexandre Marques dos Santos <dsantos@pentaho.com>
2018-09-10 15:44:39 +0100

    [BACKLOG-25482] Making sure ThinDriver is loaded in CDA OSGI environment

--- osgi/src/main/java/org/pentaho/ctools/cda/connections/dataservices/DataservicesDriverLocalConnectionProvider.java
+++ osgi/src/main/java/org/pentaho/ctools/cda/connections/dataservices/DataservicesDriverLocalConnectionProvider.java
@@ -10,31 +10,19 @@
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
  * the license for the specific language governing your rights and limitations.
  */
-
 package org.pentaho.ctools.cda.connections.dataservices;
 
-import org.pentaho.di.core.database.Database;
-import org.pentaho.di.core.exception.KettleDatabaseException;
-import org.pentaho.di.trans.dataservice.client.DataServiceConnectionInformation;
+import org.pentaho.di.trans.dataservice.jdbc.ThinDriver;
 import org.pentaho.reporting.engine.classic.core.modules.misc.datafactory.sql.DriverConnectionProvider;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class DataservicesDriverLocalConnectionProvider extends DriverConnectionProvider {
-  @Override
-  public Connection createConnection( String user, String password ) throws SQLException {
-    try {
-      return getConnection( null );
-    } catch ( KettleDatabaseException e ) {
-      throw new SQLException( e );
-    }
+
+  // Initializing the ThinDriver here in order to trigger its own static initialization block
+  // and also make it available in the DriverConnectionProvider's ClassLoader when we are creating
+  // a new connection. This will bypass DriverManager.getConnection(...) that would throw an exception
+  // before trying to connect to the ThinDriver, making it impossible to connect to it.
+  static {
+    new ThinDriver();
   }
 
-  private Connection getConnection( String dataServiceName ) throws KettleDatabaseException {
-    DataServiceConnectionInformation connectionInformation = new DataServiceConnectionInformation( dataServiceName, null, null );
-    Database database = new Database( null, connectionInformation.getDatabaseMeta() );
-    database.normalConnect( null );
-    return database.getConnection();
-  }
 }
